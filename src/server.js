b648d10048ba69c40e7c51d194e22bb9c8d17df0
Tarathep Siripis <tarathep.siripis@mail.kmutt.ac.th>
2023-02-03 10:39:09 +0700

    Add Node.JS server with Express

---
+++ src/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import viewEngine from './config/viewEngine';
+import initWebRoutes from './routes/web';
+
+let app = express();
+
+// config view engine
+viewEngine(app);
+
+// config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// init all web routes
+initWebRoutes(app);
+
+// init webhooks routes
+initWebRoutes(app);
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`App is running at port ${port}`);
+});
+
+
