b27131409bd1bc520de0a8fa0bf66b286fca9648
Tarathep Siripis <tarathep.siripis@mail.kmutt.ac.th>
2023-02-09 17:18:50 +0700

    Update Call extension getMessage

--- src/controllers/extensionController.js
+++ src/controllers/extensionController.js
@@ -26,22 +26,39 @@ let postWebhook = (req, res) => {
     // Parse the request body from the POST
     let body = req.body;
 
-    // Check the webhook event is from a Page subscription
-    if (body.object === "page") {
+    if (body.entry[0].messaging[0].message != undefined) {
+        // console.log("body", body.entry[0].messaging[0].message);
+        if (body.object === "page") {
 
-        // Iterate over each entry - there may be multiple if batched
-        body.entry.forEach(function (entry) {
-            /// Gets the body of the webhook event
-            let webhook_event = entry.messaging[0];
-            console.log("test" ,webhook_event, "test2");
+            // Iterate over each entry - there may be multiple if batched
+            body.entry.forEach(function (entry) {
+                let webhook_event = entry.messaging[0];
+
+                /// Gets the body of the webhook event
+                let sender_psid = webhook_event.sender.id;
+                // console.log('Sender PSID: ' + sender_psid);                
+                
+                const ExtensionInstaller = require('../../../FacebookMessenger/index.js');
+                const newExtensionInstaller = new ExtensionInstaller()
+
+                newExtensionInstaller.initialization({
+                    access_token: 'EAAHFfNt92vYBAGxRzMsleZCmSp60DvvHepXt302KuiHZAFCcUZCnc6kcDDpZAXAeZB7pmNZCmy5ZCWNE35TgZBOmTkI9XsgHisZBjOnLoGAFCsScgSxGk0s0IkE10pF0JDzemAMqSsRls8kH1c8SOrMmou2hKiwLPiyfFeUOEnpANzFzKA8SrN9pcbohduZBLAizkZD',
+                    pageId: '109025378769446'
+                })
+
+                // Check if the event is a message or postback and
+                // pass the event to the appropriate handler function
+                if (webhook_event.message) {
+                    // handleMessage(sender_psid, webhook_event.message);
+                    callExtensionGetMessage(webhook_event);
+                } else if (webhook_event.postback) {
+                    handlePostback(sender_psid, webhook_event.postback);
+                }
 
 
-            // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            // console.log('Sender PSID: ' + sender_psid);
-        });
-
-        // Return a '200 OK' response to all events
+            });
+            // Return a '200 OK' response to all events
+        }
         res.status(200).send("EVENT_RECEIVED");
     } else {
         // Return a '404 Not Found' if event is not from a page subscription
@@ -49,8 +66,36 @@ let postWebhook = (req, res) => {
     }
 };
 
+function callExtensionGetMessage(webhook_event) {
+    const Extension = require('../../../FacebookMessenger/index.js');
+    const newExtension = new Extension();
+
+    newExtension.initialization({
+        access_token: 'EAAHFfNt92vYBAGxRzMsleZCmSp60DvvHepXt302KuiHZAFCcUZCnc6kcDDpZAXAeZB7pmNZCmy5ZCWNE35TgZBOmTkI9XsgHisZBjOnLoGAFCsScgSxGk0s0IkE10pF0JDzemAMqSsRls8kH1c8SOrMmou2hKiwLPiyfFeUOEnpANzFzKA8SrN9pcbohduZBLAizkZD',
+        pageId: '109025378769446'
+    })
+
+    newExtension.execute({
+        function_name: "getMessages",
+        execute_data: webhook_event
+    });
+}
+
 // Handles messages events
-function handleMessage(sender_psid, received_message) { }
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) { }
